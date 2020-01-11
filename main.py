4656ed43a2a6681117ad21e32241bf3c0916510e
ramusz1 <ramusz984@gmail.com>
2020-01-12 00:34:55 +0100

    all components ready, yet no solution found by cplex solver

--- main.py
+++ main.py
@@ -7,31 +7,27 @@ sys.path.append("/opt/ibm/ILOG/CPLEX_Studio_Community129/cplex/python/3.7/x86-64
 import cplex
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
-def set_mixed_integer_problem(problem, X, y, k, first_order_solution):
+
+def set_mixed_integer_problem(problem, X, y, k, beta_approximation):
     problem.set_problem_name("integer lasso")
     problem.objective.set_sense(problem.objective.sense.minimize)
 
     n = X.shape[0]
     p = X.shape[1]
 
-    # if k == 0 return zeros ... 
-
-    # M = 2*np.max(np.abs(first_order_solution))
-    M = 100
-    z = (first_order_solution != 0) * 1
+    M = 2 * np.max(np.abs(beta_approximation))
     
     #   min         x^T Q x + c^T x
     #   subject to  Ax <= b
     #               l <= x <= u
     #               some x_i's binary or integral
 
-    Q = X.T @ X
-    c = - 2 * X.T @ y
-
     I = np.eye(p)
     rvec = np.concatenate( (np.zeros(p), np.ones(p)) )
-    # A adds the SOS-1 constraint, cplex has built in sos TODO
+    # A adds the SOS-1 constraint (b_i, 1 - z_i)
+    # cplex has built in sos TODO? (this might be hard because of 1 - z_i subtraction)
     # TODO: add beta norm < M_l to A 
     A = np.vstack((np.hstack((I, -M * I)), 
                    np.hstack((-I, -M * I)),
@@ -40,58 +36,82 @@ def set_mixed_integer_problem(problem, X, y, k, first_order_solution):
     rhs = np.concatenate((np.zeros(2*p), [k]))
     ub = np.concatenate((np.full(p, M), np.ones(p)))
     lb = np.concatenate((np.full(p, -M), np.zeros(p)))
+    Q = X.T @ X
+    c = - X.T @ y
+    # add zeros because of z vector 
+    Q = np.hstack((np.vstack((Q, np.zeros((p,p)) )),
+                   np.zeros((2*p, p)) ))
     obj = np.concatenate((c, np.zeros(p)))
-    var_types = "C" * p + "B" * p
-    # TODO start = ...
 
-    # https://stackoverflow.com/questions/38949055/linear-and-quadratic-terms-in-cplex-objective-function
-    qmat = [[[x for x in range(p)], row.tolist()] for row in Q]
-    problem.objective.set_quadratic(qmat)
+    z_approximation = (beta_approximation != 0) * 1
+    starting_point = np.concatenate((beta_approximation, z_approximation))
 
+    var_types = "C" * p + "C" * p
     var_names = [f'b{i}' for i in range(p)] + [f'z{i}' for i in range(p)]
-    
-    # lb = [ -cplex.infinity] * var_num
     problem.variables.add(obj=obj.tolist(), lb=lb, ub=ub, types=var_types, names=var_names)
 
-    indices = np.arange(2*p).tolist()
-    A_as_sparse_pairs = [cplex.SparsePair(ind=indices, val=row.tolist()) for row in A]
+    all_indices = np.arange(2*p).tolist()
+    A_as_sparse_pairs = [cplex.SparsePair(ind=all_indices, val=row.tolist()) for row in A]
     problem.linear_constraints.add(lin_expr=A_as_sparse_pairs, rhs=rhs.tolist(), senses=senses)
 
+    # initial problem solution
+    problem.MIP_starts.add(
+        cplex.SparsePair(ind=all_indices, val=starting_point.tolist()),
+        problem.MIP_starts.effort_level.auto,
+        "first_order_solution")
 
-def keep_top_k(x, k ):
-    ind = np.argsort(np.abs(x))[-k:]
+    qmat = [[all_indices, row.tolist()] for row in Q]
+    problem.objective.set_quadratic(qmat)
+
+
+
+def keep_top_k(x, k):
+    ind = np.argsort(np.abs(x))[:-k]
     x[ind] = 0
     return x
 
 
+def lm(X, y):
+    return LinearRegression().fit(X, y).coef_
+
+
+def largest_eigen_value(X, max_iter=100):
+    b = np.random.randn(X.shape[1])
+    for _ in range(max_iter):
+        v = X @ b
+        b = v / np.linalg.norm(v)
+    
+    return np.linalg.norm(v) / np.linalg.norm(b)
+
+
 # projected gradients
-def get_first_order_solution(problem, X, y, k):
+def get_first_order_solution(problem, X, y, k, n_runs=50, max_iter=1000, tolerance=1e-4):
     n = X.shape[0]
     p = X.shape[1]
 
     # init output (beta)
-    if ( p < n ){
-        # beta0 = lsfit .. #TODO
-    } else {
+    if  p < n :
+        beta0 = lm(X,y)
+    else:
         beta0 = X.t @ y / np.sum(X * X, axis=1)
-    }
+
     beta0 = keep_top_k(beta0, k) 
-    L = # power method TODO
+    L = largest_eigen_value(X.T @ X)
 
     # projected gradient runs 
     beta = beta0
-    beta_crit = np.inf
+    best_crit = np.inf
     for _ in range(n_runs):
         for _ in range(max_iter):
             beta_old = beta
             
             # grad descent step:
-            grad = -X.T @ (y - X@beta)
+            grad = -X.T @ (y - X @ beta)
             beta = beta - grad/L 
 
             beta = keep_top_k(beta, k)
 
-            if numpy.linalg.norm(beta - beta_old) / np.max((numpy.linalg.norm(), 1)) < tol:
+            if np.linalg.norm(beta - beta_old) / np.max((np.linalg.norm(beta), 1)) < tolerance:
                 break
 
         curr_crit = np.sum( (y - X @ beta)**2 )
@@ -99,7 +119,9 @@ def get_first_order_solution(problem, X, y, k):
             best_crit = curr_crit
             best_beta = beta
         
-        beta = beta0 + 2 * np.random.rand(p) * np.max((np.abs(beta0), np.ones()))
+        beta = beta0 + 2 * np.random.rand(p) * np.max((np.abs(beta0), np.ones(p)), axis=0)
+
+    return best_beta
 
 
 def best_subset(X, y, k):
@@ -107,8 +129,12 @@ def best_subset(X, y, k):
     p = cplex.Cplex()
 
     first_order_solution = get_first_order_solution(p, X, y, k)
+    
+    print(first_order_solution)
 
     set_mixed_integer_problem(p, X, y, k, first_order_solution)    
+
+    p.write("best_subset.lp")
 
     sol = p.solution
 
@@ -128,8 +154,13 @@ def best_subset(X, y, k):
     for j in range(numcols):
         print("Column %d:  Value = %10f" % (j, sol.get_values(j)))
 
-    p.write("miqpex1.lp")
+    p.write("best_subset.lp")
 
+
+def standarize(X, y):
+    # Paper requires some standarization
+    # TODO ?
+    pass
 
 if __name__ == "__main__":
     n = 100
@@ -138,7 +169,6 @@ if __name__ == "__main__":
     X = np.random.rand(n, p)
     # beta = np.random.randint(low=0, high=2,size=p)
     beta = np.array([1, 0, 1, 0, 1])
-    print(beta)
     # intercept = np.random.rand()
     # noise = np.random.random(n) * 0.01
     y = X @ beta # + intercept + noise
