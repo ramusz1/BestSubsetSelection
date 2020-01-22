9fcc1f95ac59b84126c8f98fc9f71aeccf24b930
ramusz1 <ramusz984@gmail.com>
2020-01-23 00:55:14 +0100

    cplex implementation of upper bound big M from bertsimas

--- main.py
+++ main.py
@@ -4,25 +4,18 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 
-def set_mixed_integer_problem(problem, X, y, k, beta_approximation):
+
+def set_mixed_integer_problem(problem, X, y, k, beta_approximation, M):
     problem.set_problem_name("integer lasso")
     problem.objective.set_sense(problem.objective.sense.minimize)
 
     n = X.shape[0]
     p = X.shape[1]
 
-    M = 2 * np.max(np.abs(beta_approximation))
-    
-    #   min         x^T Q x + c^T x
-    #   subject to  Ax <= b
-    #               l <= x <= u
-    #               some x_i's binary or integral
-
     I = np.eye(p)
     rvec = np.concatenate( (np.zeros(p), np.ones(p)) )
     # A adds the SOS-1 constraint (b_i, 1 - z_i)
     # cplex has built in sos TODO? (this might be hard because of 1 - z_i subtraction)
-    # TODO: add beta norm < M_l to A 
     A = np.vstack((np.hstack((I, -M * I)), 
                    np.hstack((-I, -M * I)),
                    rvec))
@@ -37,9 +30,6 @@ def set_mixed_integer_problem(problem, X, y, k, beta_approximation):
                    np.zeros((2*p, p)) ))
     obj = np.concatenate((c, np.zeros(p)))
 
-    z_approximation = (beta_approximation != 0) * 1
-    starting_point = np.concatenate((beta_approximation, z_approximation))
-
     var_types = "C" * p + "B" * p
     var_names = [f'b{i}' for i in range(p)] + [f'z{i}' for i in range(p)]
     problem.variables.add(obj=obj.tolist(), lb=lb, ub=ub, types=var_types, names=var_names)
@@ -48,11 +38,15 @@ def set_mixed_integer_problem(problem, X, y, k, beta_approximation):
     A_as_sparse_pairs = [cplex.SparsePair(ind=all_indices, val=row.tolist()) for row in A]
     problem.linear_constraints.add(lin_expr=A_as_sparse_pairs, rhs=rhs.tolist(), senses=senses)
 
-    # initial problem solution
-    problem.MIP_starts.add(
-        cplex.SparsePair(ind=all_indices, val=starting_point.tolist()),
-        problem.MIP_starts.effort_level.auto,
-        "first_order_solution")
+    if beta_approximation is not None:
+        z_approximation = (beta_approximation != 0) * 1
+        starting_point = np.concatenate((beta_approximation, z_approximation))
+
+        # initial problem solution
+        problem.MIP_starts.add(
+            cplex.SparsePair(ind=all_indices, val=starting_point.tolist()),
+            problem.MIP_starts.effort_level.auto,
+            "first_order_solution")
 
     qmat = [[all_indices, row.tolist()] for row in Q]
     problem.objective.set_quadratic(qmat)
@@ -68,6 +62,11 @@ def lm(X, y):
     return LinearRegression().fit(X, y).coef_
 
 
+def get_fast_beta_approx(X, y, k):
+    beta = lm(X,y)
+    return keep_top_k(beta, k) 
+
+
 def largest_eigen_value(X, max_iter=100):
     b = np.random.randn(X.shape[1])
     for _ in range(max_iter):
@@ -78,17 +77,12 @@ def largest_eigen_value(X, max_iter=100):
 
 
 # projected gradients
-def get_first_order_solution(problem, X, y, k, n_runs=50, max_iter=1000, tolerance=1e-4):
+def get_first_order_solution(X, y, k, n_runs=50, max_iter=1000, tolerance=1e-4):
     n = X.shape[0]
     p = X.shape[1]
 
     # init output (beta)
-    if  p < n :
-        beta0 = lm(X,y)
-    else:
-        beta0 = X.T @ y / np.sum(X * X, axis=0)
-
-    beta0 = keep_top_k(beta0, k) 
+    beta0 = get_fast_beta_approx(X, y, k)
     L = largest_eigen_value(X.T @ X)
 
     # projected gradient runs 
@@ -118,12 +112,79 @@ def get_first_order_solution(problem, X, y, k, n_runs=50, max_iter=1000, toleran
     return best_beta
 
 
-def best_subset(X, y, k):
+def get_mild_start(X, y, k):
+    beta_approximation = get_fast_beta_approx(X, y, k) 
+    mild_start_tau = 5
+    big_M = mild_start_tau * np.max(np.abs(beta_approximation))
+    return beta_approximation, big_M
+
+
+def abs_corr(x, y):
+    return np.corrcoef(x, y)[0][1]
+
+
+def get_miu(X):
+    X_T = X.T
+    max_corr = -np.inf
+    for i in range(X_T.shape[0]):
+        for j in range(i + 1, X_T.shape[0]):
+            tmp = abs_corr(X_T[i], X_T[j])
+            max_corr = np.max((tmp, max_corr))
+
+    return max_corr
+
+
+def theory_driven_big_M(X, y, k):
+    miu = get_miu(X)
+    if miu * (k - 1) < 1:
+        raise ValueError("miu[k-1] < 1, switching to mild start")
+    gamma_k = 1 - miu * (k - 1) # minimum bound on gamma_k
+    print("debugsssss")
+    print(miu)
+    print(gamma_k)
+
+    ## first expression
+    X_T = X.T
+    correlations = np.zeros(X_T.shape[0])
+    for i in range(X_T.shape[0]):
+        correlations[i] = abs_corr(X_T[i], y)
+    
+    k_highest_correlations = - np.partition( - correlations, k)[:k]
+    k_highest_correlations = k_highest_correlations ** 2
+    first_expr = 1 / gamma_k * np.sqrt(np.sum(correlations))
+
+    ## second expression
+    second_expr = 1 / np.sqrt(gamma_k) * np.linalg.norm(y, ord=2)
+
+    big_M = min(first_expr, second_expr)
+    return big_M
+
+
+def best_subset(X, y, k, start = 'warm'):
+
+    if X.shape[0] < X.shape[1]:
+        print("WARNING, case p > n is not implemented yet")
 
     p = cplex.Cplex()
+    if 'warm' == start:
+        beta_approximation = get_first_order_solution(X, y, k)
+        warm_start_tau = 2
+        big_M = warm_start_tau * np.max(np.abs(beta_approximation))
+    elif 'mild' == start:
+        beta_approximation, big_M = get_mild_start(X, y, k) 
+    elif 'cold' == start:
+        try:
+            big_M = theory_driven_big_M(X, y, k)
+            beta_approximation = None
+        except ValueError as v: #TODO better handling? 
+            print(v)
+            beta_approximation, big_M = get_mild_start(X, y, k) 
+        
+    else :
+        raise ValueError("start should be one of: warm, mild, cold")
 
-    first_order_solution = get_first_order_solution(p, X, y, k)
-    set_mixed_integer_problem(p, X, y, k, first_order_solution)    
+    set_mixed_integer_problem(p, X, y, k, beta_approximation, big_M)
+
     p.solve()
     sol = p.solution
     # solution.get_status() returns an integer code
@@ -146,23 +207,23 @@ def best_subset(X, y, k):
 
     miqp_solution_beta = np.array(miqp_solution)[:X.shape[1]]
 
-    return first_order_solution, miqp_solution_beta
+    return beta_approximation, miqp_solution_beta
 
 
-def standarize(X, y, k):
-    # Paper requires some standarization
-    # TODO ?
-    pass
+def run(X, y, k):
+    y = y.flatten()
+
+    beta_approximation, miqp_solution = best_subset(X, y, k)
+    return beta_approximation, miqp_solution
 
 
-def final_error(beta_pred, beta_true):
-    return np.sum( (beta_true - beta_pred)**2 )
-
-def run(X, beta, k):
-    # TODO intercept handling
-    beta_py = beta.flatten()
-    y = X @ beta_py # + intercept + noise
-
-    # first_order_solution, miqp_solution = best_subset(X, y, k = np.sum(beta) )
-    first_order_solution, miqp_solution = best_subset(X, y, int(k))
-    return first_order_solution, miqp_solution
+if __name__ == '__main__':
+    n = 100
+    p = 10
+    X = np.random.rand(n, p)
+    beta = np.random.rand(p) * 3
+    y = X @ beta 
+    k = 5
+    for start in ['warm', 'mild', 'cold']:
+        beta_approximation, miqp_solution = best_subset(X, y, k, start)
+        print(beta_approximation, miqp_solution)
